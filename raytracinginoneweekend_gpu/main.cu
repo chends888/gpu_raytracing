f69606d10aea464308ad677df044b416ab6e5e0d
Lucas Alba <lchends@gmail.com>
2019-06-05 20:15:55 -0300

    Changes chapter 5

--- raytracinginoneweekend_gpu/main.cu
+++ raytracinginoneweekend_gpu/main.cu
@@ -1,3 +1,5 @@
+// Código adaptado de:
+// https://devblogs.nvidia.com/accelerated-ray-tracing-cuda/
 #include <iostream>
 #include <fstream>
 #include <float.h>
@@ -18,23 +20,6 @@ void check_cuda(cudaError_t result, char const *const func, const char *const fi
     }
 }
 
-// __device__ bool hit_sphere(const vec3& center, float radius, const ray& r) {
-//     vec3 oc = r.origin() - center;
-//     float a = dot(r.direction(), r.direction());
-//     float b = 2.0f * dot(oc, r.direction());
-//     float c = dot(oc, oc) - radius*radius;
-//     float discriminant = b*b - 4.0f*a*c;
-//     return (discriminant > 0.0f);
-// }
-
-// __device__ vec3 color(const ray& r) {
-//     if (hit_sphere(vec3(0,0,-1), 0.5, r))
-//         return vec3(1,0,0);
-//     vec3 unit_direction = unit_vector(r.direction());
-//     float t = 0.5f*(unit_direction.y() + 1.0f);
-//     return (1.0f-t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
-// }
-
 __device__ vec3 color(const ray& r, hitable **world) {
     hit_record rec;
     if ((*world)->hit(r, 0.0, FLT_MAX, rec)) {
@@ -48,14 +33,15 @@ __device__ vec3 color(const ray& r, hitable **world) {
 }
 
 
-__global__ void render(vec3 *fb, int max_x, int max_y, vec3 lower_left_corner, vec3 horizontal, vec3 vertical, vec3 origin, hitable **world) {
+// Defining GPU kernel
+__global__ void render(vec3 *fb, int width, int height, vec3 lower_left_corner, vec3 horizontal, vec3 vertical, vec3 origin, hitable **world) {
     int i = threadIdx.x + blockIdx.x * blockDim.x;
     int j = threadIdx.y + blockIdx.y * blockDim.y;
     // Check if block id is within block size
-    if ((i < max_x) && (j < max_y)) {
-        int pixel_index = j*max_x + i;
-        float u = float(i) / float(max_x);
-        float v = float(j) / float(max_y);
+    if ((i < width) && (j < height)) {
+        int pixel_index = j*width + i;
+        float u = float(i) / float(width);
+        float v = float(j) / float(height);
         ray r(origin, lower_left_corner + u*horizontal + v*vertical);
         fb[pixel_index] = color(r, world);
     }
@@ -64,8 +50,9 @@ __global__ void render(vec3 *fb, int max_x, int max_y, vec3 lower_left_corner, v
 
 __global__ void create_world(hitable **d_list, hitable **d_world) {
     if (threadIdx.x == 0 && blockIdx.x == 0) {
+        // Generate two spheres
         *(d_list)   = new sphere(vec3(0,0,-1), 0.5);
-        *(d_list+1) = new sphere(vec3(0,-100.5,-1), 100);
+        *(d_list+1) = new sphere(vec3(0,-50.5,-1), 100);
         *d_world    = new hitable_list(d_list,2);
     }
 }
@@ -83,25 +70,27 @@ int main() {
     int ny = 50;
     int num_pixels = nx*ny;
     size_t fb_size = 3*num_pixels*sizeof(vec3);
-    // Allocate for individual pixels in GPU
     vec3 *fb;
+    // Allocate frame buffer for individual pixels in GPU
     checkCudaErrors(cudaMallocManaged((void **)&fb, fb_size));
 
 
-    // Make our world of hitables
+    // Create world of hitables
     hitable **d_list;
     checkCudaErrors(cudaMalloc((void **)&d_list, 2*sizeof(hitable *)));
     hitable **d_world;
     checkCudaErrors(cudaMalloc((void **)&d_world, sizeof(hitable *)));
-    create_world<<<1,1>>>(d_list,d_world);
+    create_world<<<1,1>>>(d_list, d_world);
     checkCudaErrors(cudaGetLastError());
     checkCudaErrors(cudaDeviceSynchronize());
 
+    // Define number of threads per block
     int tx = 8;
     int ty = 8;
-    // Render the buffer on GPU
-    dim3 blocks(nx/tx+1,ny/ty+1);
     dim3 threads(tx,ty);
+    // Define block size
+    dim3 blocks(nx/tx+1,ny/ty+1);
+    // Render image on GPU
     render<<<blocks, threads>>>(fb, nx, ny,
                                 vec3(-2.0, -1.0, -1.0),
                                 vec3(4.0, 0.0, 0.0),
@@ -114,11 +103,9 @@ int main() {
     std::ofstream myfile;
     myfile.open ("image.ppm");
     myfile << "P3\n" << nx << " " << ny << "\n255\n";
+    // Get pixels value
     for (int j = ny-1; j >= 0; j--) {
         for (int i = 0; i < nx; i++) {
-            // float r = float(i) / float (nx);
-            // float g = float(j) / float (ny);
-            // float b = 0.2;
             size_t pixel_index = j*nx + i;
             int ir = int(255.99*fb[pixel_index].r());
             int ig = int(255.99*fb[pixel_index].g());
@@ -127,7 +114,7 @@ int main() {
             myfile << ir << " " << ig << " " << ib << "\n";
         }
     }
-    // clean up
+    // Free used memory and check errors
     checkCudaErrors(cudaDeviceSynchronize());
     free_world<<<1,1>>>(d_list,d_world);
     checkCudaErrors(cudaGetLastError());
